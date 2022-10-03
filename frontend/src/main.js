a32db7a52d6b1875305688361baac693e9b90370
Vito Ramadhan <98963921+tejuslemonnn@users.noreply.github.com>
2022-10-03 13:48:31 +0700

    repair vue & tailwind

---
+++ frontend/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Vue from 'vue'
+import VueTailwind from 'vue-tailwind'
+
+Vue.use(VueTailwind)
+createApp(App).mount('#app')
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    variants: {
+      extend: {
+        opacity: ['disabled'],
+        cursor: ['disabled'],
+      },
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+    ],
+  };
+  
